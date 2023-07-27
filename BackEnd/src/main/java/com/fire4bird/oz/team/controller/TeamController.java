e39a1ffa48acedfa4cf964a2ba410d4d26d8e634
김주하 <tkfkd122@naver.com>
2023-07-28 03:18:21 +0900

    S09P12B104-212 Feat: Controller regist 추가

--- BackEnd/src/main/java/com/fire4bird/oz/team/controller/TeamController.java
+++ BackEnd/src/main/java/com/fire4bird/oz/team/controller/TeamController.java
@@ -1,13 +1,16 @@
 package com.fire4bird.oz.team.controller;
 
 
+import com.fire4bird.oz.CMRespDto;
 import com.fire4bird.oz.team.dto.CheckUserDto;
+import com.fire4bird.oz.team.dto.RegistTeamDto;
 import com.fire4bird.oz.team.entity.Team;
 import com.fire4bird.oz.team.mapper.TeamMapper;
 import com.fire4bird.oz.team.service.TeamService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,15 +21,20 @@ import org.springframework.web.bind.annotation.*;
 public class TeamController {
 
     private final TeamService teamService;
-    private final TeamMapper teamMapper;
 
     // 4명의 조합이 있던 조합인지 확인
     @PostMapping("/checkteam")
-    public ResponseEntity checkTeam(@RequestBody CheckUserDto checkUserDto, HttpServletResponse response) {
+    public ResponseEntity checkTeam(@RequestBody CheckUserDto checkUserDto) {
         Team findTeam = teamService.findTeam(checkUserDto.getUsers());
 
-        response.setHeader("TeamResult", findTeam.getTeamName());
+        return new ResponseEntity<>(new CMRespDto<>(1, "유저 팀 확인 완료", findTeam), HttpStatus.OK);
+    }
 
-        return ResponseEntity.ok("팀 이름 찾음");
+
+    @PostMapping("/registteam")
+    public ResponseEntity registTeam(@RequestBody RegistTeamDto registTeamDto){
+        Team startTeam = teamService.checkTeamName(registTeamDto);
+
+        return new ResponseEntity<>(new CMRespDto<>(1, "팀 구성 완료", startTeam), HttpStatus.OK);
     }
 }
