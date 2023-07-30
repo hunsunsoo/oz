b2827031e846ed53a2184d4066b0673cffa848a2
한성현 <hsh5349@kakao.com>
2023-07-30 21:50:11 +0900

    Feat: 배포 테스트 컨트롤러 생성

---
+++ BackEnd/src/main/java/com/fire4bird/oz/DeployTestController.java
@@ -0,0 +1,14 @@
+package com.fire4bird.oz;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeployTestController {
+
+    @GetMapping("/")
+    public ResponseEntity deployTest() {
+        return ResponseEntity.ok("제발 쫌...");
+    }
+}
