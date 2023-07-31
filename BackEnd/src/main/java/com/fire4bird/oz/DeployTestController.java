b155bc0ed2f98f550389ef8e3702e54ec23cc5d5
한성현 <hsh5349@kakao.com>
2023-07-31 21:03:13 +0900

    Modify: ssl 인증 https 테스트

--- BackEnd/src/main/java/com/fire4bird/oz/DeployTestController.java
+++ BackEnd/src/main/java/com/fire4bird/oz/DeployTestController.java
@@ -2,13 +2,20 @@ package com.fire4bird.oz;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class DeployTestController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public ResponseEntity deployTest() {
-        return ResponseEntity.ok("제발 쫌...");
+        return ResponseEntity.ok("ssl 인증서 테스트");
+    }
+
+    @GetMapping("/test2")
+    public ResponseEntity deployTest2() {
+        return ResponseEntity.ok("제발제발제발제발제발제발제발제발제발제발제발제발제발");
     }
 }
