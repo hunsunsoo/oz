6d3dd4345b4f2dbd0d4bb6ce452a32d7e66b0683
한성현 <hsh5349@naver.com>
2023-08-02 01:32:53 +0900

    S09P12B104-270 Modify: 사용하지 않는 import 제거

--- BackEnd/src/main/java/com/fire4bird/oz/config/RedisConfig.java
+++ BackEnd/src/main/java/com/fire4bird/oz/config/RedisConfig.java
@@ -2,7 +2,6 @@ package com.fire4bird.oz.config;
 
 import com.fire4bird.oz.socket.service.RedisSubscriber;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
