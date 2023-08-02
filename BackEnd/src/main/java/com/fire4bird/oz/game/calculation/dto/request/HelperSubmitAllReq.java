d890c58ea8eae7476c98ac1cb277baf26d20e3e0
김주하 <tkfkd122@naver.com>
2023-08-02 12:47:44 +0900

    S09P12B104-218 Feat: 조력자 선택 블록 전체 request 생성

---
+++ BackEnd/src/main/java/com/fire4bird/oz/game/calculation/dto/request/HelperSubmitAllReq.java
@@ -0,0 +1,11 @@
+package com.fire4bird.oz.game.calculation.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelperSubmitAllReq {
+    private Integer gameId;
+    private String selectedNums;
+}
