2ba3e42bc154288bbeab188000ab9873d4a39d0f
김주하 <tkfkd122@naver.com>
2023-08-02 12:43:33 +0900

    S09P12B104-218 Feat: repository 생성

---
+++ BackEnd/src/main/java/com/fire4bird/oz/game/calculation/repository/CalculationRepository.java
@@ -0,0 +1,12 @@
+package com.fire4bird.oz.game.calculation.repository;
+
+
+import com.fire4bird.oz.game.calculation.entity.Calculation;
+import com.fire4bird.oz.round.entity.Round;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CalculationRepository extends JpaRepository<Calculation, Integer> {
+    Optional<Calculation> findByRound_RoundId(Integer roundId);
+}
