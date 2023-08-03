903675b3703159c84de0fd077dbcfe40220a2fc9
한성현 <hsh5349@kakao.com>
2023-08-03 15:12:15 +0900

    S09P12B104-274 Feat: 블랙리스트 토큰 조회

--- BackEnd/src/main/java/com/fire4bird/oz/jwt/blacklist/service/BlackListService.java
+++ BackEnd/src/main/java/com/fire4bird/oz/jwt/blacklist/service/BlackListService.java
@@ -1,11 +1,15 @@
 package com.fire4bird.oz.jwt.blacklist.service;
 
+import com.fire4bird.oz.error.BusinessLogicException;
+import com.fire4bird.oz.error.ExceptionCode;
 import com.fire4bird.oz.jwt.blacklist.key.BlackList;
 import com.fire4bird.oz.jwt.blacklist.mapper.BlackListMapper;
 import com.fire4bird.oz.jwt.blacklist.repository.BlackListRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +23,15 @@ public class BlackListService {
         BlackList blackList = mapping(accessToken);
         blackListRepository.save(blackList);
         log.info("엑세스 토큰 블랙리스트에 추가");
+    }
+
+    //블랙리스트에서 엑세스토큰 조회
+    public void findBlackList(String accessToken) {
+        Optional<BlackList> findBlackList = blackListRepository.findById(accessToken);
+
+        if (findBlackList.isPresent()) {
+            throw new BusinessLogicException(ExceptionCode.TOKEN_NOT_VALID);
+        }
     }
 
     //매핑
