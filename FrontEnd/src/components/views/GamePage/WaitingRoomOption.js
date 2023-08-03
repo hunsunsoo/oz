17427188b7e13be9fa25d3b5d5129f601b3132f8
DongIkkk <w8h0412@gmail.com>
2023-08-04 02:20:30 +0900

    S09P12B104-229-frontBase Feat: 대기방 구성 및 Front 흐름 제어

---
+++ FrontEnd/src/components/views/GamePage/WaitingRoomOption.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const WaitingRoomOption = ({ isWaiting, onGamingStart }) => {
+
+  const handleGamingStartState = () => {
+    const newStatus = !isWaiting
+    onGamingStart(newStatus);
+  };
+
+  const WROStyle = {
+    backgroundColor: "red",
+    height: "20%",
+    display: "flex",
+  };
+
+  const leftPaneStyle = {
+    flex: "1",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const rightPaneStyle = {
+    flex: "1",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const buttonStyle = {
+    padding: "10px 20px",
+    fontSize: "30px",
+    backgroundColor: "blue",
+    color: "white",
+    border: "none",
+    cursor: "pointer",
+    borderRadius: "5px",
+    margin: "10px",
+  };
+
+  return (
+    <div style={WROStyle}>
+      <div style={leftPaneStyle}>
+        {/* Left Pane Content */}
+        <button style={buttonStyle}>친구 초대하기</button>
+      </div>
+      <div style={rightPaneStyle}>
+        {/* Right Pane Content */}
+        <button style={buttonStyle} onClick={handleGamingStartState}>
+          모험시작
+        </button>
+        <button style={buttonStyle}>나가기</button>
+      </div>
+    </div>
+  );
+};
+
+export default WaitingRoomOption;
