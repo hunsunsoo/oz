9bb368bfbf3074c84904864fdd92d3db4ab8f5c4
함소명 <gkathaud@naver.com>
2023-08-04 10:10:27 +0900

    S09P12B104-247 Modify: RoleSelect 로직 수정

--- BackEnd/src/main/java/com/fire4bird/oz/socket/service/RoleService.java
+++ BackEnd/src/main/java/com/fire4bird/oz/socket/service/RoleService.java
@@ -23,38 +23,42 @@ public class RoleService {
      */
     public SocketRoleDto roleSelect(SocketMessage message, SocketRoleDto socketRoleDto) {
         int selectRole = socketRoleDto.getRole();
+        int userId = message.getUserId();
         String msg = "";
         int state = -1;
-        if(roles.containsKey(message.getRtcSession())){
+        if (roles.containsKey(message.getRtcSession())) {
             int[] role = roles.get(message.getRtcSession());
-            int selectedRole = role[message.getUserId()];
+            int getRole = checkRole(role, userId);
             //유저 역할 선택시
-            if(socketRoleDto.getState()==1){
-                //이미 선택한 경우
-                if(selectedRole!=0){
-                    msg = message.getUserId() + "님은 이미 "+selectedRole+"역할을 선택하였습니다.";
-                }else{
-                //선택하지 않은 경우
-                    //선택 할 수 있는 경우
-                    if(checkRole(role, selectRole)){
-                        role[message.getUserId()] = selectRole;
-                        msg = message.getUserId() + "님이 "+selectRole+"역할을 선택하였습니다.";
+            if (socketRoleDto.getState() == 1) {
+                //이미 선택된 경우
+                if (role[selectRole] != 0) msg = selectRole + "역할은 이미 선택되었습니다.";
+                else {
+                //선택되지 않은 경우
+                    //유저가 다른 역할을 선택했는지 확인
+                    if (getRole == 0) {
+                        role[selectRole] = userId;
                         state = 1;
-                    }else {
-                    //다른 유저가 선택한 경우
-                        msg = selectRole + "역할은 이미 다른 유저가 선택하였습니다.";
-                    }
+                        msg = userId + "님이 " + selectRole + "역할을 선택하였습니다.";
+                    } else msg = userId + "님은 이미 역할을 선택하였습니다.";
                 }
-            }else {
-            //유저 역할 취소
-                role[message.getUserId()] = 0;
-                msg = message.getUserId() + "님이 "+selectRole+"역할을 취소하였습니다.";
+            } else {
+                //유저 역할 취소시
+                if (getRole != 0) {
+                    //갖고있는 역할이 있음
+                    //선택한 역할과 갖고있는 역할이 같다면 취소
+                    if (selectRole == getRole) {
+                        state = 1;
+                        role[getRole] = 0;
+                        msg = userId + "님이 갖고 있는 역할을 취소하였습니다.";
+                    } else msg = userId + "님은 " + selectRole + "역할을 갖고있지 않습니다."
+                } else msg = userId + "님이 갖고 있는 역할이 없어 취소할 수 없습니다.";
             }
             roles.replace(message.getRtcSession(), role);
-        }else{
-            int[] role = new int[4];
-            role[message.getUserId()] = selectRole;
-            msg = message.getUserId() + "님이 "+selectRole+"역할을 선택하였습니다.";
+        } else {
+            int[] role = new int[5];
+            role[selectRole] = userId;
+            msg = userId + "님이 " + selectRole + "역할을 선택하였습니다.";
             state = 1;
             roles.put(message.getRtcSession(), role);
         }
@@ -63,13 +67,12 @@ public class RoleService {
         return socketRoleDto;
     }
 
-    private boolean checkRole(int[] role, int selectId){
-        boolean check = true;
-        for(int roleId : role){
-            if(roleId==selectId)
-                check = false;
+    private int checkRole(int[] role, int userId) {
+        int check = 0;
+        for (int i = 1; i < role.length; i++) {
+            if (userId == role[i])
+                check = i;
         }
         return check;
     }
-
 }
