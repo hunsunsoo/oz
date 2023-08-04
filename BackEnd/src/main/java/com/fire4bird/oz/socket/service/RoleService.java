99ffed73d91cc9a014bc9b60f447d64f5f3304ac
함소명 <gkathaud@naver.com>
2023-08-04 17:58:38 +0900

    S09P12B104-247 Modify: Round 처리 수정

--- BackEnd/src/main/java/com/fire4bird/oz/socket/service/RoleService.java
+++ BackEnd/src/main/java/com/fire4bird/oz/socket/service/RoleService.java
@@ -29,13 +29,9 @@ public class RoleService {
         if (roles.containsKey(message.getRtcSession())) {
             int[] role = roles.get(message.getRtcSession());
             int getRole = checkRole(role, userId);
-            //유저 역할 선택시
             if (socketRoleDto.getState() == 1) {
-                //이미 선택된 경우
                 if (role[selectRole] != 0) msg = selectRole + "역할은 이미 선택되었습니다.";
                 else {
-                //선택되지 않은 경우
-                    //유저가 다른 역할을 선택했는지 확인
                     if (getRole == 0) {
                         role[selectRole] = userId;
                         state = 1;
@@ -43,15 +39,12 @@ public class RoleService {
                     } else msg = userId + "님은 이미 역할을 선택하였습니다.";
                 }
             } else {
-                //유저 역할 취소시
                 if (getRole != 0) {
-                    //갖고있는 역할이 있음
-                    //선택한 역할과 갖고있는 역할이 같다면 취소
                     if (selectRole == getRole) {
                         state = 1;
                         role[getRole] = 0;
                         msg = userId + "님이 갖고 있는 역할을 취소하였습니다.";
-                    } else msg = userId + "님은 " + selectRole + "역할을 갖고있지 않습니다."
+                    } else msg = userId + "님은 " + selectRole + "역할을 갖고있지 않습니다.";
                 } else msg = userId + "님이 갖고 있는 역할이 없어 취소할 수 없습니다.";
             }
             roles.replace(message.getRtcSession(), role);
