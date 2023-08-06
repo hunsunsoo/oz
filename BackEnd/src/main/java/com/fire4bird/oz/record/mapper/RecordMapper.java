fd71146bba7393b6ebd23f8acab610297e28ee4e
한성현 <hsh5349@naver.com>
2023-08-06 15:13:53 +0900

    Feat: mapper 생성

--- BackEnd/src/main/java/com/fire4bird/oz/record/mapper/RecordMapper.java
+++ BackEnd/src/main/java/com/fire4bird/oz/record/mapper/RecordMapper.java
@@ -4,7 +4,10 @@ import com.fire4bird.oz.record.entity.Record;
 import com.fire4bird.oz.round.entity.Round;
 import org.mapstruct.Mapper;
 
+import java.time.LocalDateTime;
+
 @Mapper(componentModel = "spring")
 public interface RecordMapper {
-    Record toEntity(Round round, int stageNum);
+
+    Record toEntity(Round round, int stageNum, LocalDateTime startTime);
 }
