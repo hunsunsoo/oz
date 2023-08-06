ca68c7ee0151e5d3396b4462c5168f44c7608364
한성현 <hsh5349@naver.com>
2023-08-06 22:38:45 +0900

    Feat: 해당 회차의 모든 플레이타임 조회 쿼리 생성

--- BackEnd/src/main/java/com/fire4bird/oz/record/repository/RecordRepositoryImpl.java
+++ BackEnd/src/main/java/com/fire4bird/oz/record/repository/RecordRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.fire4bird.oz.record.repository;
 import com.fire4bird.oz.record.entity.Record;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,6 +12,7 @@ import static com.fire4bird.oz.record.entity.QRecord.record;
 import static com.fire4bird.oz.round.entity.QRound.round;
 
 @RequiredArgsConstructor
+@Slf4j
 public class RecordRepositoryImpl implements RecordRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
@@ -33,6 +35,17 @@ public class RecordRepositoryImpl implements RecordRepositoryCustom {
                 .from(record)
                 .join(record.round, round).on(round.roundId.eq(roundId))
                 .where(record.stageNum.eq(stageNum))
+                .fetch();
+    }
+
+    @Override
+    public List<LocalDateTime> findByClearRecord(int roundId) {
+        return jpaQueryFactory
+                .select(record.accRecord)
+                .from(record)
+                .join(record.round, round).on(round.roundId.eq(roundId))
+                .where(record.clear.eq("clear"),
+                        record.accRecord.isNotNull())
                 .fetch();
     }
 }
