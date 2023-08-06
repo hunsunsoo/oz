ad600743e5e0465d0fe06d43d0b56877b02f3a6c
DongIkkk <w8h0412@gmail.com>
2023-08-06 22:44:16 +0900

    Feat: 4명의 유저 아이디로 진행한 팀이 있는지 조회/검증

--- FrontEnd/src/components/views/GamePage/WaitingRoomOption.js
+++ FrontEnd/src/components/views/GamePage/WaitingRoomOption.js
@@ -6,7 +6,7 @@ const WaitingRoomOption = ({ isWaiting, onGamingStart, userId, sessionId, amIHos
 
   const OPENVIDU_SERVER_URL = "https://i9b104.p.ssafy.io:8443";
   const OPENVIDU_SERVER_SECRET = "MY_SECRET";
-  const SERVER_URL = 'http://localhost:8080/'
+  const SERVER_URL = 'http://localhost:8080'
 
   const handleGamingStartState = () => {
     if(amIHost == 1) {
@@ -25,14 +25,38 @@ const WaitingRoomOption = ({ isWaiting, onGamingStart, userId, sessionId, amIHos
                 "참가 인원(4명)이 충족되지 않았습니다. 시작이 불가능합니다."
               );
             } else {
-              axios.post(`${SERVER_URL}socket/user?rtcSession=${sessionId}`,{}
+              axios.post(`${SERVER_URL}/socket/user?rtcSession=${sessionId}`,{}
               ).then((response) => {
                 const userIdArray = response.data.data.map(obj => obj.userId);
-                
+                axios.post(`${SERVER_URL}/teams/checkteam`,{
+                  "users" : userIdArray
+                }
+                ).then((response) => {
+                  console.log(response.data.data);
+                  const teamNameDefault = response.data.data.teamName;
+                  var teamName;
+                  if (teamNameDefault === null){
+                    teamName = prompt("사용할 팀 이름을 정하세요");
+                  } else {
+                    teamName = prompt(`해당 4명의 사용자는 기존에 사용하던 팀 명이 있습니다. ${teamNameDefault} 이거 쓰던거 쓰려면 아무것도 입력하지말고 넘기시고 새로 팀만들고싶으면 적으세요 이건나중에 바꿀거에요`, teamNameDefault )
+                  }
+                  console.log(teamName+"이름으로 팀만들었고 다음페이지로 넘어가는거 만드는중")
+                  axios.post(`${SERVER_URL}/teams/registteam`, {
+                    "users" : userIdArray,
+                    "teamName" : `${teamName}`
+                  })
+                  .then(response => {
+                    
+                    console.log(response.data);
+                  })
+                  .catch(error => {
+                    console.error(error);
+                  });
+                }
+                );
+                })
               }
-              );
-            }
-          })
+            })
           .catch((error) => {
             console.log("방 정보를 가져오는데 실패하였습니다.", error);
           });
