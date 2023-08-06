a347b93720406da71fd4e27589b3e0e7daad34db
한성현 <hsh5349@naver.com>
2023-08-06 15:13:38 +0900

    Feat: 스테이지 시작 시 기록 저장 api 생성

--- BackEnd/src/main/java/com/fire4bird/oz/record/controller/RecordController.java
+++ BackEnd/src/main/java/com/fire4bird/oz/record/controller/RecordController.java
@@ -1,12 +1,12 @@
 package com.fire4bird.oz.record.controller;
 
+import com.fire4bird.oz.common.CMRespDto;
 import com.fire4bird.oz.record.dto.RecordRegistDto;
-import com.fire4bird.oz.record.entity.Record;
 import com.fire4bird.oz.record.service.RecordService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecordController {
     private final RecordService recordService;
 
-    //테스트 컨트롤러
-    @GetMapping("/test")
+    //스테이지 시작
+    //게임 시작 시간 및 도전 횟수 저장
+    @PostMapping("/start")
     public ResponseEntity test(@RequestBody RecordRegistDto recordRegistDto) {
-        Record record = recordService.findRecord(recordRegistDto.getRoundId(), recordRegistDto.getStageNum());
         recordService.saveRecord(recordRegistDto.getRoundId(), recordRegistDto.getStageNum());
-        return null;
+
+        return ResponseEntity.ok()
+                .body(new CMRespDto<>(1,"기록 저장 성공",null));
     }
+
+    //스테이지 종료
+    //게임 종료 시간 및 clear 여부 저장
+
+
 }
