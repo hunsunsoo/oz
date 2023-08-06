321810035f7cfa14fdfa9e035b07e7d3c05fca87
한성현 <hsh5349@naver.com>
2023-08-06 20:30:31 +0900

    Feat: 스테이지 기록 쿼리 생성

--- BackEnd/src/main/java/com/fire4bird/oz/record/repository/RecordRepositoryCustom.java
+++ BackEnd/src/main/java/com/fire4bird/oz/record/repository/RecordRepositoryCustom.java
@@ -2,8 +2,14 @@ package com.fire4bird.oz.record.repository;
 
 import com.fire4bird.oz.record.entity.Record;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface RecordRepositoryCustom {
 
     //기록 찾아오기
     Record findByRecord(int roundId, int stageNum);
+
+    //클리어시 사용될 기록 찾아오기 - 시간 데이터만 리스트로
+    List<LocalDateTime> findByTimeRecord(int roundId, int stageNum);
 }
--- BackEnd/src/main/java/com/fire4bird/oz/record/repository/RecordRepositoryImpl.java
+++ BackEnd/src/main/java/com/fire4bird/oz/record/repository/RecordRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.fire4bird.oz.record.repository;
 
 import com.fire4bird.oz.record.entity.Record;
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static com.fire4bird.oz.record.entity.QRecord.record;
 import static com.fire4bird.oz.round.entity.QRound.round;
@@ -16,19 +18,21 @@ public class RecordRepositoryImpl implements RecordRepositoryCustom {
     @Override
     public Record findByRecord(int roundId, int stageNum) {
         return jpaQueryFactory
-                .select(Projections.fields(Record.class,
-                        record.stageNum,
-                        record.challengeTurn,
-                        record.stageRecord,
-                        record.accRecord,
-                        record.startTime,
-                        record.endTime,
-                        record.clear,
-                        record.round))
+                .select(record)
                 .from(record)
                 .join(record.round, round).on(round.roundId.eq(roundId))
                 .where(record.stageNum.eq(stageNum))
                 .orderBy(record.challengeTurn.desc())
                 .fetchFirst();
+    }
+
+    @Override
+    public List<LocalDateTime> findByTimeRecord(int roundId, int stageNum) {
+        return jpaQueryFactory
+                .select(record.stageRecord)
+                .from(record)
+                .join(record.round, round).on(round.roundId.eq(roundId))
+                .where(record.stageNum.eq(stageNum))
+                .fetch();
     }
 }
