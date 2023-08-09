d9a8126b945e723e33e87900b254d6539c956082
한성현 <hsh5349@kakao.com>
2023-08-09 17:53:21 +0900

    S09P12B104-280 Modify: 랭킹 로직 수정중

--- BackEnd/src/main/java/com/fire4bird/oz/record/repository/RecordRepository.java
+++ BackEnd/src/main/java/com/fire4bird/oz/record/repository/RecordRepository.java
@@ -1,12 +1,13 @@
 package com.fire4bird.oz.record.repository;
 
 import com.fire4bird.oz.record.entity.Record;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface RecordRepository extends JpaRepository<Record,Integer>,RecordRepositoryCustom {
+public interface RecordRepository extends JpaRepository<Record, Integer>, RecordRepositoryCustom {
 
     @Query(value = "select " +
             "    rank() over (order by min(re.acc_record)) as 'rank', " +
@@ -16,7 +17,7 @@ public interface RecordRepository extends JpaRepository<Record,Integer>,RecordRe
             "    record re " +
             "        join round r on r.round_id = re.round_id " +
             "        join team t on t.team_id = r.team_id " +
-            "where stage_num = 1 " +
+            "where stage_num = :stageNum " +
             "group by t.team_name;", nativeQuery = true)
-    List<Object[]> test();
+    List<Object[]> findTotalRankTest(@Param("stageNum") int stageNum);
 }
