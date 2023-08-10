bdc3951592a72aa89d88d956b75dd5671b7bf971
한성현 <hsh5349@kakao.com>
2023-08-10 17:25:11 +0900

    S09P12B104-315 Feat: 순차적으로 플레이하지 않을 경우 예외 추가

--- BackEnd/src/main/java/com/fire4bird/oz/error/record/RecordError.java
+++ BackEnd/src/main/java/com/fire4bird/oz/error/record/RecordError.java
@@ -3,6 +3,7 @@ package com.fire4bird.oz.error.record;
 import com.fire4bird.oz.error.BusinessLogicException;
 import com.fire4bird.oz.error.ExceptionCode;
 import com.fire4bird.oz.record.entity.Record;
+import com.fire4bird.oz.record.repository.RecordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,8 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 public class RecordError {
+
+    private final RecordRepository recordRepository;
 
     //유효성 검사 - 스테이지 도전 시
     public void startRecordValid(Record findRecord) {
@@ -37,9 +40,21 @@ public class RecordError {
     }
 
     //스테이지 번호 유효성 검사
-    public void stageNumValid(int stageNum) {
+    public void stageNumValid(int stageNum,int roundId) {
         if (stageNum < 1 || stageNum > 4) {
             throw new BusinessLogicException(ExceptionCode.STAGE_NUM_NOT_VALID);
+        }
+
+        noRecentRecord(stageNum,roundId);
+    }
+
+    //이전 스테이지를 클리어 하지 않았을 경우 플레이 불가
+    public void noRecentRecord(int stageNum, int roundId) {
+        if (stageNum != 1) {
+            Record findRecord = recordRepository.findByRecord(roundId, stageNum - 1,"clear");
+
+            Optional.ofNullable(findRecord)
+                    .orElseThrow(() -> new BusinessLogicException(ExceptionCode.PREVIOUS_STAGE_NO_CLEAR));
         }
     }
 }
