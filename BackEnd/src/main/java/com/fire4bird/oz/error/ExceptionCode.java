e6766c9d525960c2e1b546a749fd529d95c91de2
한성현 <hsh5349@kakao.com>
2023-08-10 17:24:10 +0900

    S09P12B104-315 Feat: 에러 enum 추가

--- BackEnd/src/main/java/com/fire4bird/oz/error/ExceptionCode.java
+++ BackEnd/src/main/java/com/fire4bird/oz/error/ExceptionCode.java
@@ -4,24 +4,34 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 public enum ExceptionCode {
-    /** 유저 관련 **/
+    /**
+     * 유저 관련
+     **/
     DUPLICATE_ID(HttpStatus.CONFLICT.value(), "중복된 아이디가 존재합니다."),
-    BAD_PARAM(HttpStatus.NOT_FOUND.value(),"아이디 혹은 비밀번호가 틀렸습니다."),
+    BAD_PARAM(HttpStatus.NOT_FOUND.value(), "아이디 혹은 비밀번호가 틀렸습니다."),
     PASSWORD_NOT_VALID(HttpStatus.BAD_REQUEST.value(), "비밀번호가 틀렸습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(),"해당 회원을 찾을 수 없습니다."),
-    TOKEN_NOT_VALID(HttpStatus.UNAUTHORIZED.value(), "토큰이 유효하지 않습니다.") ,
-    CODE_NOT_VALID(HttpStatus.BAD_REQUEST.value(),"잘 못 된 인증 코드입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 회원을 찾을 수 없습니다."),
+    TOKEN_NOT_VALID(HttpStatus.UNAUTHORIZED.value(), "토큰이 유효하지 않습니다."),
+    CODE_NOT_VALID(HttpStatus.BAD_REQUEST.value(), "잘 못 된 인증 코드입니다."),
 
-    /** 모험 시작 **/
+    /**
+     * 모험 시작
+     **/
     TEAM_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "팀을 찾을 수 없습니다."),
     VALIDATION_FAIL(HttpStatus.BAD_REQUEST.value(), "유효검사 실패하였습니다."),
     FORBIDDEN_OWNER(HttpStatus.FORBIDDEN.value(), "방장이 아닙니다."),
 
-    /** 웹 소켓 **/
-    SOCKET_MESSAGE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(),"웹 소켓 메시지 전달에 실패하였습니다."),
+    /**
+     * 웹 소켓
+     **/
+    SOCKET_MESSAGE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "웹 소켓 메시지 전달에 실패하였습니다."),
 
-    /** 기록 **/
+    /**
+     * 기록
+     **/
     BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), "잘 못 된 요청입니다."),
+
+    PREVIOUS_STAGE_NO_CLEAR(HttpStatus.BAD_REQUEST.value(), "이전 스테이지가 클리어 되지 않았습니다"),
     STAGE_NUM_NOT_VALID(HttpStatus.BAD_REQUEST.value(), "잘 못 된 스테이지 번호입니다."),
     NO_CHALLENGE_RECORD(HttpStatus.BAD_REQUEST.value(), "도전 기록이 없습니다.");
     @Getter
