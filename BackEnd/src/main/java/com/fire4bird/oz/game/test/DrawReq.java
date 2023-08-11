4c618e9917ee39cda4ed103be8183f826b131674
김주하 <tkfkd122@naver.com>
2023-08-11 15:46:20 +0900

    S09P12B104-242 Feat: 이어그리기 테스트 코드

---
+++ BackEnd/src/main/java/com/fire4bird/oz/game/test/DrawReq.java
@@ -0,0 +1,14 @@
+package com.fire4bird.oz.game.test;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DrawReq {
+    private String sessionId;
+    private int x;
+    private int y;
+    private float width;
+    private String color;
+}
