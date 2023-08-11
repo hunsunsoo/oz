e87a77e5753b530dd8f907911fda2021a562d93c
한성현 <hsh5349@kakao.com>
2023-08-11 13:02:23 +0900

    Modify: 웹 소켓 설정 변경

--- BackEnd/src/main/java/com/fire4bird/oz/config/WebStompConfigurer.java
+++ BackEnd/src/main/java/com/fire4bird/oz/config/WebStompConfigurer.java
@@ -18,7 +18,7 @@ public class WebStompConfigurer implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/ws").setAllowedOrigins("*")
+        registry.addEndpoint("/api/ws").setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 }
