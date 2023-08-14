17b129400e35761420720dbb1fd950a6e8d62478
gangins <kki129443@gmail.com>
2023-08-14 09:42:59 +0900

    revive Modify: Feat: Modal

--- FrontEnd/src/components/views/GamePage/GameComps/PuzzleGame/PuzzleReady.js
+++ FrontEnd/src/components/views/GamePage/GameComps/PuzzleGame/PuzzleReady.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import GameModal from "../GameModal/GameModal";
-import style from "./PuzzleReady.module.css"
-
-const PuzzleReady = ({ myRole, onHandleStart, client, sessionId, R1,R2,R3,R4 }) => {
+import style from "./PuzzleReady.module.css";
+import CustomAlert from "../Alert/alert";
+const PuzzleReady = ({
+  myRole,
+  onHandleStart,
+  client,
+  sessionId,
+  R1,
+  R2,
+  R3,
+  R4,
+}) => {
   const stageval = 3;
   const [showModal, setShowModal] = useState(false);
 
@@ -13,75 +22,75 @@ const PuzzleReady = ({ myRole, onHandleStart, client, sessionId, R1,R2,R3,R4 }) 
 
   const [amIHost, setAmIHost] = useState(host);
   const [amIReady, setAmIReady] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
 
   useEffect(() => {
-    if(myRole === 1){
-      if(R1 === 3){
+    if (myRole === 1) {
+      if (R1 === 3) {
         setAmIReady(true);
-      } else if(R1 === 0){
+      } else if (R1 === 0) {
         setAmIReady(false);
       }
-    } else if(myRole === 2){
-      if(R2 === 3){
+    } else if (myRole === 2) {
+      if (R2 === 3) {
         setAmIReady(true);
-      } else if(R2 === 0){
+      } else if (R2 === 0) {
         setAmIReady(false);
       }
-    } else if(myRole === 3){
-      if(R3 === 3){
+    } else if (myRole === 3) {
+      if (R3 === 3) {
         setAmIReady(true);
-      } else if(R3 === 0){
+      } else if (R3 === 0) {
         setAmIReady(false);
       }
-    } else if(myRole === 4){
-      if(R4 === 3){
+    } else if (myRole === 4) {
+      if (R4 === 3) {
         setAmIReady(true);
-      } else if(R4 === 0){
+      } else if (R4 === 0) {
         setAmIReady(false);
       }
-    } 
+    }
   }, [myRole, R1, R2, R3, R4]);
 
   const handleStartAfterReady = () => {
-    console.log(host)
-    if(amIHost === "1"){
-      if(R1 === 3 && R2 === 3 && R3 === 3 && R4 === 3){
+    console.log(host);
+    if (amIHost === "1") {
+      if (R1 === 3 && R2 === 3 && R3 === 3 && R4 === 3) {
         onHandleStart(true);
       } else {
-        alert("4명이 준비 완료 상태가 아닙니다");
+        setAlertMessage("4명이 준비 완료 상태가 아닙니다");
       }
     } else {
-      alert("내가 방장이 아니다.");
+      setAlertMessage("내가 방장이 아니다.");
     }
   };
 
-  const ReadyPublisher = async (readyOrCancel) => {  
+  const ReadyPublisher = async (readyOrCancel) => {
     try {
       if (!client) {
-        console.log('웹소켓이 연결중이 아닙니다. 메시지 보내기 실패');
+        console.log("웹소켓이 연결중이 아닙니다. 메시지 보내기 실패");
         return;
       }
 
       const message = {
-        "type":"ready",
-        "rtcSession":`${sessionId}`,
-        "role":myRole,
-        "stage":3,
-        "state":readyOrCancel,
-        "message":`${myRole}님이 ${readyOrCancel} (1준비 0취소)하셨습니다`
+        type: "ready",
+        rtcSession: `${sessionId}`,
+        role: myRole,
+        stage: 3,
+        state: readyOrCancel,
+        message: `${myRole}님이 ${readyOrCancel} (1준비 0취소)하셨습니다`,
       };
 
-      client.send('/pub/socket/ready', {}, JSON.stringify(message));
-      console.log('메시지 보냈음');
+      client.send("/pub/socket/ready", {}, JSON.stringify(message));
+      console.log("메시지 보냈음");
     } catch (error) {
-      console.log('Error sending message:', error);
+      console.log("Error sending message:", error);
     }
   };
-  
+
   const onHandleExplain = () => {
     setShowModal(true);
   };
-
 
   return (
     <div className={style.compStyle}>
@@ -90,80 +99,87 @@ const PuzzleReady = ({ myRole, onHandleStart, client, sessionId, R1,R2,R3,R4 }) 
           <div className={style.firstDiv}>
             <img
               src="image/character/twm_light.png"
-              className={style.checkMain}>
-            </img>
+              className={style.checkMain}
+            ></img>
             {R4 === 3 && (
               <img
-              src="image/tools/checkmarker.png"
-              className={style.checkMarkExtra}>
-            </img>
+                src="image/tools/checkmarker.png"
+                className={style.checkMarkExtra}
+              ></img>
             )}
           </div>
           <div className={style.otherDiv}>
             <img
               src="image/character/dorothy.png"
-              className={style.checkExtra}>
-            </img>
+              className={style.checkExtra}
+            ></img>
             {R1 === 3 && (
               <img
-              src="image/tools/checkmarker.png"
-              className={style.checkMarkExtra}>
-            </img>
+                src="image/tools/checkmarker.png"
+                className={style.checkMarkExtra}
+              ></img>
             )}
           </div>
           <div className={style.otherDiv}>
             <img
               src="image/character/liona.png"
-              className={style.checkExtra}>
-            </img>
+              className={style.checkExtra}
+            ></img>
             {R2 === 3 && (
               <img
-              src="image/tools/checkmarker.png"
-              className={style.checkMarkExtra}>
-            </img>
+                src="image/tools/checkmarker.png"
+                className={style.checkMarkExtra}
+              ></img>
             )}
           </div>
           <div className={style.otherDiv}>
             <img
               src="image/character/heosua.png"
-              className={style.checkExtra}>
-            </img>
+              className={style.checkExtra}
+            ></img>
             {R3 === 3 && (
               <img
-              src="image/tools/checkmarker.png"
-              className={style.checkMarkExtra}>
-            </img>
+                src="image/tools/checkmarker.png"
+                className={style.checkMarkExtra}
+              ></img>
             )}
           </div>
         </div>
         <div className={style.guideStyle}>
           <div className={style.topDivStyle}>
-            <div className={style.howToPlayImg}>
-              게임 방법 넣을 part
-            </div>
+            <div className={style.howToPlayImg}>게임 방법 넣을 part</div>
           </div>
           <div className={style.bottomDivStyle}>
-            <div className={style.howToPlayBtn} onClick={onHandleExplain} >
-                게임 방법
-              </div>
-              
-              <div className={style.readyBtn} onClick={() => {
-                                              const readyOrCancel = amIReady ? 0 : 1;
-                                              ReadyPublisher(readyOrCancel);
-                                            }}>
-                {amIReady ? "준비 취소" : "준비 완료"}
-              </div>
-
-              {amIHost === "1" && (
-                <div className={style.startBtn} onClick={handleStartAfterReady}>
-                  게임 시작
-                </div>
-              )}
+            <div className={style.howToPlayBtn} onClick={onHandleExplain}>
+              게임 방법
             </div>
+
+            <div
+              className={style.readyBtn}
+              onClick={() => {
+                const readyOrCancel = amIReady ? 0 : 1;
+                ReadyPublisher(readyOrCancel);
+              }}
+            >
+              {amIReady ? "준비 취소" : "준비 완료"}
+            </div>
+
+            {amIHost === "1" && (
+              <div className={style.startBtn} onClick={handleStartAfterReady}>
+                게임 시작
+              </div>
+            )}
+          </div>
         </div>
       </div>
       {showModal && (
         <GameModal isStage={stageval} closeModal={() => setShowModal(false)} />
+      )}
+      {alertMessage && (
+        <CustomAlert
+          message={alertMessage}
+          onClose={() => setAlertMessage("")}
+        />
       )}
     </div>
   );
