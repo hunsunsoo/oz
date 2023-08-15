4d26d3eca49cfd2f18d0d57721ec07131970c393
한성현 <hsh5349@kakao.com>
2023-08-15 18:12:33 +0900

    TEST:TEST

--- FrontEnd/src/_actions/urls.js
+++ FrontEnd/src/_actions/urls.js
@@ -4,4 +4,4 @@ export const OPENVIDU_SERVER_URL = "https://i9b104.p.ssafy.io:8443";
 // export const WEBSOCKET_SERVER_URL = "ws://localhost:8080/ws";
 
 export const SERVER_URL = 'https://i9b104.p.ssafy.io/api'
-export const WEBSOCKET_SERVER_URL = 'wss://i9b104.p.ssafy.io/ws';
+export const WEBSOCKET_SERVER_URL = 'wss://i9b104.p.ssafy.io/api/ws';
