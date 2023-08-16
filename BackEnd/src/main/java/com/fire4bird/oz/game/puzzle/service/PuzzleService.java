b754bd7c24a9c6623d553a6b2a5953a743857a27
한성현 <hsh5349@kakao.com>
2023-08-16 11:03:56 +0900

    S09P12B104-356 Feat: 퍼즐 시작 및 종료 시 기록 저장

--- BackEnd/src/main/java/com/fire4bird/oz/game/puzzle/service/PuzzleService.java
+++ BackEnd/src/main/java/com/fire4bird/oz/game/puzzle/service/PuzzleService.java
@@ -1,10 +1,6 @@
 package com.fire4bird.oz.game.puzzle.service;
 
-import com.fire4bird.oz.error.BusinessLogicException;
-import com.fire4bird.oz.error.ExceptionCode;
 import com.fire4bird.oz.game.puzzle.dto.req.PuzzleAnswerReq;
-import com.fire4bird.oz.game.puzzle.dto.req.PuzzleLogReq;
-import com.fire4bird.oz.game.puzzle.dto.req.PuzzleReadyReq;
 import com.fire4bird.oz.game.puzzle.dto.req.PuzzleStartReq;
 import com.fire4bird.oz.game.puzzle.entity.Puzzle;
 import com.fire4bird.oz.game.puzzle.entity.PuzzleLog;
@@ -12,6 +8,7 @@ import com.fire4bird.oz.game.puzzle.manager.PuzzleGameManager;
 import com.fire4bird.oz.game.puzzle.mapper.PuzzleMapper;
 import com.fire4bird.oz.game.puzzle.repository.PuzzleLogRepository;
 import com.fire4bird.oz.game.puzzle.repository.PuzzleRepository;
+import com.fire4bird.oz.record.service.RecordService;
 import com.fire4bird.oz.round.entity.Round;
 import com.fire4bird.oz.round.repository.RoundRepository;
 import com.fire4bird.oz.socket.dto.RedisSaveObject;
@@ -20,13 +17,14 @@ import com.fire4bird.oz.socket.service.RedisPublisher;
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PuzzleService {
     private final SocketRepository socketRepository;
     private final RoundRepository roundRepository;
@@ -35,12 +33,13 @@ public class PuzzleService {
     private final PuzzleMapper puzzleMapper;
     private final PuzzleRepository puzzleRepository;
     private final PuzzleLogRepository puzzleLogRepository;
+    private final RecordService recordService;
 
     private PuzzleGameManager puzzleGameManager;
 
 
     @PostConstruct
-    public void init(){
+    public void init() {
         puzzleGameManager = new PuzzleGameManager(socketRepository, redisPublisher, puzzleMapper);
     }
 
@@ -49,14 +48,24 @@ public class PuzzleService {
     public void gameStart(PuzzleStartReq req) {
         //방장인지 확인
         int owner = socketRepository.findOwnerById(req.getRtcSession());
-        if(req.getUserId()!=owner) return;
+        if (req.getUserId() != owner) return;
 
-        RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(),String.valueOf(req.getUserId()));
+        RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(), String.valueOf(req.getUserId()));
         Round findRound = roundRepository.findById(obj.getRoundId()).orElseThrow(RuntimeException::new);
         Puzzle puzzle = puzzleGameManager.statGame(req, findRound, 1);
 
+        log.info("방장id : {}", owner);
+        log.info("db에서 찾아온 라운드 id : {}", findRound.getRoundId());
+        log.info("받아온 라운드 id : {}}", obj.getRoundId());
+        //게임 시작 시 기록 타이밍
+        //유효성 체크
+        recordService.validRequest(3, findRound.getRoundId(), owner);
+
+        //기록 저장
+        recordService.saveStartRecord(findRound.getRoundId(), 3);
+
         //type 1: 게임 시작 로그
-        saveLog(req.getRtcSession(),req.getUserId(),0,1, "Start Answer: "+puzzle.getAnswer()+", Board: "+puzzle.getBoard());
+        saveLog(req.getRtcSession(), req.getUserId(), 0, 1, "Start Answer: " + puzzle.getAnswer() + ", Board: " + puzzle.getBoard());
 
         puzzleRepository.save(puzzle);
     }
@@ -64,38 +73,48 @@ public class PuzzleService {
     //게임 다시 시작(오답 시)
     @Transactional
     public void gameRetry(PuzzleStartReq req) {
-        RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(),String.valueOf(req.getUserId()));
+        RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(), String.valueOf(req.getUserId()));
         Round findRound = roundRepository.findById(obj.getRoundId()).orElseThrow(RuntimeException::new);
 
-        int turn = puzzleRepository.maxTurn(obj.getRoundId()).getTurn()+1;
+        int turn = puzzleRepository.maxTurn(obj.getRoundId()).getTurn() + 1;
         Puzzle puzzle = puzzleGameManager.statGame(req, findRound, turn);
 
         //type 2: 게임 다시 시작 로그 저장
-        saveLog(req.getRtcSession(),req.getUserId(),0,2, "Retry answer: "+ puzzle.getAnswer());
+        saveLog(req.getRtcSession(), req.getUserId(), 0, 2, "Retry answer: " + puzzle.getAnswer());
 
         puzzleRepository.save(puzzle);
     }
 
     @Transactional
-    public void saveLog(String rtcSession, int userid, int system, int logType, String msg){
-        RedisSaveObject obj = socketRepository.findRoundById(rtcSession,String.valueOf(userid));
+    public void saveLog(String rtcSession, int userid, int system, int logType, String msg) {
+        RedisSaveObject obj = socketRepository.findRoundById(rtcSession, String.valueOf(userid));
         Round findRound = roundRepository.findById(obj.getRoundId()).orElseThrow(RuntimeException::new);
 
-        PuzzleLog puzzleLog = puzzleMapper.sendPuzzleLog(userid,system,logType,msg,LocalDateTime.now(),findRound);
+        PuzzleLog puzzleLog = puzzleMapper.sendPuzzleLog(userid, system, logType, msg, LocalDateTime.now(), findRound);
         puzzleLogRepository.save(puzzleLog);
     }
 
     @Transactional
-    public void gameAnswer(PuzzleAnswerReq req){
-        RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(),String.valueOf(req.getUserId()));
+    public void gameAnswer(PuzzleAnswerReq req) {
+        RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(), String.valueOf(req.getUserId()));
         Puzzle puzzle = puzzleRepository.maxTurn(obj.getRoundId());
 
         int check = -1;
-        if(req.getUserAnswer().equals(puzzle.getAnswer())) check = 1;
-        puzzleGameManager.publisher(req.getRtcSession(), "puzzle/data","게임 정답 확인",check);
+        if (req.getUserAnswer().equals(puzzle.getAnswer())) check = 1;
+        puzzleGameManager.publisher(req.getRtcSession(), "puzzle/data", "게임 정답 확인", check);
+
+        log.info("라운드 id : {}", obj.getRoundId());
+
+        //게임 종료 기록 저장 타이밍
+        if (check == 1) {
+            recordService.clearCheck(obj.getRoundId(), 3, "clear");
+        } else {
+            recordService.clearCheck(obj.getRoundId(), 3, "false");
+        }
+
 
         //type 4: 게임 정답 제출 로그
-        saveLog(req.getRtcSession(),req.getUserId(),0,4, "Answer Submit: "+req.getUserAnswer()+", Check: "+ check);
+        saveLog(req.getRtcSession(), req.getUserId(), 0, 4, "Answer Submit: " + req.getUserAnswer() + ", Check: " + check);
 
         puzzle.setUserAnswer(req.getUserAnswer());
         puzzle.setIsCheck(check);
