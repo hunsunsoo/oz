53ce93c7075f80bd7fda90ef2313c0205931dedc
gangins <kki129443@gmail.com>
2023-08-16 09:40:33 +0900

    S09P12B104-343-front-css-수정완료 Modify: prettier exit

--- FrontEnd/src/_actions/urls.js
+++ FrontEnd/src/_actions/urls.js
@@ -3,5 +3,6 @@ export const OPENVIDU_SERVER_URL = "https://i9b104.p.ssafy.io:8443";
 // export const SERVER_URL = "http://localhost:8080";
 // export const WEBSOCKET_SERVER_URL = "ws://localhost:8080/ws";
 
-export const SERVER_URL = "https://i9b104.p.ssafy.io/api";
-export const WEBSOCKET_SERVER_URL = "wss://i9b104.p.ssafy.io/ws";
+export const SERVER_URL = 'https://i9b104.p.ssafy.io/api'
+export const WEBSOCKET_SERVER_URL = 'wss://i9b104.p.ssafy.io/api/ws';
+
