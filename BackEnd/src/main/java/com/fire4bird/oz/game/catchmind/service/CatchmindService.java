4fb66a991defc1f70acc04d016378322356154e6
한성현 <hsh5349@kakao.com>
2023-08-16 09:59:16 +0900

    S09P12B104-356 Feat: 4스테이지 종료 시 기록 저장

--- BackEnd/src/main/java/com/fire4bird/oz/game/catchmind/service/CatchmindService.java
+++ BackEnd/src/main/java/com/fire4bird/oz/game/catchmind/service/CatchmindService.java
@@ -1,7 +1,9 @@
 package com.fire4bird.oz.game.catchmind.service;
 
 import com.fire4bird.oz.game.catchmind.dto.DrawingDto;
-import com.fire4bird.oz.game.catchmind.dto.req.*;
+import com.fire4bird.oz.game.catchmind.dto.req.AnswerReq;
+import com.fire4bird.oz.game.catchmind.dto.req.PassReq;
+import com.fire4bird.oz.game.catchmind.dto.req.StartReq;
 import com.fire4bird.oz.game.catchmind.entity.Drawing;
 import com.fire4bird.oz.game.catchmind.entity.DrawingData;
 import com.fire4bird.oz.game.catchmind.entity.DrawingLog;
@@ -37,18 +39,19 @@ public class CatchmindService {
     private CatchmindGameManager catchmindGameManager;
 
 
-
     @PostConstruct
-    public void init() { catchmindGameManager = new CatchmindGameManager(socketRepository, redisPublisher, catchmindDataRepository); }
+    public void init() {
+        catchmindGameManager = new CatchmindGameManager(socketRepository, redisPublisher, catchmindDataRepository);
+    }
 
     // 게임 준비 확인
     public void gameStart(StartReq req) {
         int owner = socketRepository.findOwnerById(req.getRtcSession());
-        if(req.getUserId()!=owner) return;
+        if (req.getUserId() != owner) return;
 
         RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(), String.valueOf(req.getUserId()));
         Round findRound = roundRepository.findById(obj.getRoundId()).orElseThrow(RuntimeException::new);
-        long turn = catchmindRepository.countTurn(obj.getRoundId())+1;
+        long turn = catchmindRepository.countTurn(obj.getRoundId()) + 1;
         DrawingData drawingData = catchmindGameManager.startGame(req);
 
         //기록 유효성 검사 및 기록 테이블에 등록
@@ -64,49 +67,61 @@ public class CatchmindService {
         Drawing drawing = Drawing.builder()
                 .catchmindData(drawingData)
                 .answer(drawingData.getAnswer())
-                .turn((int)turn)
+                .turn((int) turn)
                 .userAnswer("")
                 .round(findRound)
                 .drawingPicture("")
                 .build();
 
         // type 1: 게임 시작 로그
-        userLog(req.getRtcSession(), req.getUserId(), 0, 1, turn+"회차 제시어: "+drawing.getAnswer());
+        userLog(req.getRtcSession(), req.getUserId(), 0, 1, turn + "회차 제시어: " + drawing.getAnswer());
         catchmindRepository.save(drawing);
     }
 
     public void gameDrawing(DrawingDto req) {
-        catchmindGameManager.publisher(req.getSessionId(), "draw/drawing","그리는 중",req);
+        catchmindGameManager.publisher(req.getSessionId(), "draw/drawing", "그리는 중", req);
     }
 
     public void gamePass(PassReq req) {
-        catchmindGameManager.publisher(req.getRtcSession(), "draw/pass","턴 넘기기",req.getCurrentRole()+1);
+        catchmindGameManager.publisher(req.getRtcSession(), "draw/pass", "턴 넘기기", req.getCurrentRole() + 1);
 
         // type 2: 역할 넘기기 로그
         RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(), String.valueOf(req.getUserId()));
         long turn = catchmindRepository.countTurn(obj.getRoundId());
-        userLog(req.getRtcSession(), req.getUserId(), 0, 1, turn+"회차 "+req.getCurrentRole()+1+"역할에게 "+req.getPng()+"넘기기");
+        userLog(req.getRtcSession(), req.getUserId(), 0, 1, turn + "회차 " + req.getCurrentRole() + 1 + "역할에게 " + req.getPng() + "넘기기");
     }
 
     public void imageReset(StartReq req) {
-        catchmindGameManager.publisher(req.getRtcSession(), "draw/reset","그림 리셋",null);
+        catchmindGameManager.publisher(req.getRtcSession(), "draw/reset", "그림 리셋", null);
 
         // type 3: 리셋 로그
         RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(), String.valueOf(req.getUserId()));
         long turn = catchmindRepository.countTurn(obj.getRoundId());
-        userLog(req.getRtcSession(), req.getUserId(), 0, 1, turn+"회차 "+req.getUserId()+"가 리셋함");
+        userLog(req.getRtcSession(), req.getUserId(), 0, 1, turn + "회차 " + req.getUserId() + "가 리셋함");
     }
 
-    public void gameAnswer(AnswerReq req){
-        RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(),String.valueOf(req.getUserId()));
+    public void gameAnswer(AnswerReq req) {
+        RedisSaveObject obj = socketRepository.findRoundById(req.getRtcSession(), String.valueOf(req.getUserId()));
         Drawing catchmind = catchmindRepository.maxTurn(obj.getRoundId());
+
+        //정답 확인 후 유효성 검사
+        recordService.validRequest(4, obj.getRoundId(), obj.getUserId());
 
         int check = -1;
         if (req.getUserAnswer().equals(catchmind.getAnswer())) check = 1;
+
+        if (check == 1) {
+            recordService.clearCheck(obj.getRoundId(), 4, "clear");
+        }
+        else {
+            recordService.clearCheck(obj.getRoundId(), 4, "false");
+        }
+
         catchmindGameManager.publisher(req.getRtcSession(), "draw/data", "게임 정답 확인", check);
 
+
         // type 4: 게임 정답 제출 로그
-        userLog(req.getRtcSession(), req.getUserId(), 0, 4, "제출한 답: "+req.getUserAnswer() +", 정답 확인: "+check);
+        userLog(req.getRtcSession(), req.getUserId(), 0, 4, "제출한 답: " + req.getUserAnswer() + ", 정답 확인: " + check);
 
         catchmind.setUserAnswer(req.getUserAnswer());
         catchmind.setIsCheck(check);
@@ -115,7 +130,7 @@ public class CatchmindService {
     }
 
     public void userLog(String rtcSession, int userId, int system, int logType, String msg) {
-        RedisSaveObject obj = socketRepository.findRoundById(rtcSession,String.valueOf(userId));
+        RedisSaveObject obj = socketRepository.findRoundById(rtcSession, String.valueOf(userId));
         Round findRound = roundRepository.findById(obj.getRoundId()).orElseThrow(RuntimeException::new);
         DrawingLog catchmindLog = DrawingLog.builder()
                 .userId(userId)
